43f742dff49ca8efd607b14dd88f74d141c979d1
Dmitry Frank <mail@dmitryfrank.com>
2017-06-09 16:52:30 +0300

    Factor out rpc-service-gpio
    PUBLISHED_FROM=142ff277efac46b55150f6fc8d235b3dedd7e2a9

---
+++ src/mgos_gpio_service.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014-2016 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#ifndef CS_FW_SRC_MGOS_GPIO_SERVICE_H_
+#define CS_FW_SRC_MGOS_GPIO_SERVICE_H_
+
+#include "fw/src/mgos_features.h"
+
+#if MGOS_ENABLE_RPC
+
+#include "fw/src/mgos_init.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+bool mgos_rpc_service_gpio_init(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* MGOS_ENABLE_RPC */
+#endif /* CS_FW_SRC_MGOS_GPIO_SERVICE_H_ */
